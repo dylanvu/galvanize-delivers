2ae1c3caca0b15d703a5fa05fa23df4f304ba319
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-28 06:48:12 -0800

    Tweak

--- js/script.js
+++ js/script.js
@@ -5,8 +5,19 @@
 
   const items = [];
 
-  const renderOrder = function() {
+  $('.addItem').on('click', (event) => {
     $('tbody').empty();
+
+    const $target = $(event.target);
+    const $cardContent = $target.parent().siblings('.card-content');
+    const name = $cardContent.children('.card-title').text();
+    const price = parseFloat($cardContent.children('p').text().slice(1));
+
+    const item = {};
+    item.name = name;
+    item.price = price;
+    items.push(item);
+
     let $tr;
     let $tdName;
     let $tdPrice;
@@ -32,22 +43,6 @@
     $('#subtotal').text(`$${subtotal.toFixed(2)}`);
     $('#tax').text(`$${tax.toFixed(2)}`);
     $('#total').text(`$${total.toFixed(2)}`);
-  }
-
-  renderOrder();
-
-  $('.addItem').on('click', (event) => {
-    const $target = $(event.target);
-    const $cardContent = $target.parent().siblings('.card-content');
-    const name = $cardContent.children('.card-title').text();
-    const price = parseFloat($cardContent.children('p').text().slice(1));
-
-    const item = {};
-    item.name = name;
-    item.price = price;
-    items.push(item);
-
-    renderOrder();
   });
 
   $('#placeOrder').on('click', () => {
