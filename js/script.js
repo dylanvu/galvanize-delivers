0093ecc4046d6676d43ac28b652c018228dad8e4
Dylan Vu <dylan.n.vu@gmail.com>
2016-11-28 04:36:59 -0800

    add click handler for adding to an order

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
-$(document).ready(() => {
+(function() {
+  'use strict';
+
   $(".button-collapse").sideNav();
-});
+
+  const $tbody = $('tbody');
+
+  const items = [];
+
+  $('.addItem').on('click', (event) => {
+    const $target = $(event.target);
+    const $cardContent = $target.parent().siblings('.card-content');
+    const name = $cardContent.children('.card-title').text();
+    const price = $cardContent.children('p').text();
+
+    const $tr = $('<tr>');
+    const $tdName = $('<td>');
+    const $tdPrice = $('<td>').addClass('right-align');
+
+    $tdName.append(name);
+    $tdPrice.append(price);
+
+    $tr.append($tdName);
+    $tr.append($tdPrice);
+
+    $tbody.append($tr);
+  });
+})();
